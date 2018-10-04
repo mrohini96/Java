c323c7dc89ec83c5c86cb87e4d434f263b3aae2d
Rohini.Mallewade <Rohini.Mallewade@TRI02L-PF19UYCP.trianz.int>
2018-10-04 16:29:14 +0530

    One line changed

--- JavaPractice/src/Practice1/org/Employee.java
+++ JavaPractice/src/Practice1/org/Employee.java
@@ -3,5 +3,6 @@ package Practice1.org;
 public class Employee {
 public static void main(String[] args) {
 	System.out.println("Eclipse is running");
+	System.out.println("Added this statement");
 }
 }
